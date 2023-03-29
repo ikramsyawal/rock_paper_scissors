5900993d944e92723b92a90054d25edc12958368
Ikram Syawal Alitu <ikramsyawalalitu@gmail.com>
2023-03-29 23:34:52 +0800

    create player prompt. and game main logic

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function getComputerChoice(){
     let computerHand = ['paper','rock','scissors'];
     return computerHand[Math.floor(Math.random() * computerHand.length)];
 }
+let playerHand = prompt('What is Your hand? please input Text!').toLowerCase();
+
+
+function round(computerSelection, playerSelection){
+    if (computerSelection === playerSelection){
+        alert(`${computerSelection} vs ${playerSelection}. Its a Tie`);
+        computerScore++
+        playerScore++
+    }else if(computerSelection==='rock' && playerSelection ==='scissors'){
+        alert(`${computerSelection} vs ${playerSelection}. Computer WIN`);
+        computerScore++
+    }else if(computerSelection==='paper' && playerSelection ==='rock'){
+        alert(`${computerSelection} vs ${playerSelection}. Computer WIN`);
+        computerScore++
+    }else if(computerSelection==='scissors' && playerSelection ==='paper'){
+        alert(`${computerSelection} vs ${playerSelection}. Computer WIN`);
+        computerScore++
+    }else{
+        alert(`${computerSelection} vs ${playerSelection}. Player WIN`);
+        playerScore++
+    }
+};
